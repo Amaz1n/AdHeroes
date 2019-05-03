e23e309073b2bb98eed722368a0fdaaafd9894a1
Rick <richardzhou55@gmail.com>
2019-05-03 09:29:57 -0400

    added gravity

--- AdHeroes.py
+++ AdHeroes.py
@@ -3,13 +3,14 @@ from pygame import *
 from math import *
 from random import *
 init()
-size=width,height=600,850
+size=width,height=1000,600
 screen=display.set_mode(size)
+grav=4.4
 
 running=True
 while running:
     for evt in event.get():
         if evt.type==QUIT:
             running=False
-
+    display.flip()
 quit()
