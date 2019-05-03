e37e18e0831bd23da6af57663492a63c04a01203
PaulCho02 <50210244+PaulCho02@users.noreply.github.com>
2019-05-03 09:57:04 -0400

    Update AdHeroes.py

--- AdHeroes.py
+++ AdHeroes.py
@@ -7,10 +7,47 @@ size=width,height=1024,800
 screen=display.set_mode(size)
 grav=4.4
 
-running=True
-while running:
-    for evt in event.get():
-        if evt.type==QUIT:
-            running=False
-    display.flip()
+def menu():
+    running = True
+    myClock = time.Clock()
+    buttons=[Rect(430,200,200,60),Rect(430,300,200,60),
+             Rect(430,400,200,60),Rect(430,500,200,60)]
+    vals=["game","credits","instructions"]
+    while running:
+        for evnt in event.get():
+            if evnt.type==QUIT:
+                return "exit"
+        mx,my=mouse.get_pos()
+        mb=mouse.get_pressed()
+        #background image (screen.blit(..))
+        for i in range(len(buttons)):
+            draw.rect(screen,(0,255,0),buttons[i],2)
+            if buttons[i].collidepoint(mx,my):
+                draw.rect(screen,(0,0,255),buttons[i],2)
+                if mb[0]==1:
+                    return vals[i]
+            else:
+                draw.rect(screen,(255,255,0),buttons[i],2)
+                          
+        display.flip()
+                          
+                          
+running=True                          
+x,y=0,0        
+page = "menu"
+while page != "exit":
+    if page == "menu":
+        page = menu()
+    if page == "game":
+        page = simpleGame()    
+    if page == "instructions":
+        page = instructions()    
+    if page == "story":
+        page = story()    
+    if page == "credits":
+        page = credit()
+
+
+
+
 quit()
