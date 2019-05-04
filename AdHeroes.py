e14ff2f515ec728c6375a38924c193c4aa3b8514
PaulCho02 <50210244+PaulCho02@users.noreply.github.com>
2019-05-05 04:36:01 +0900

    Update AdHeroes.py

--- AdHeroes.py
+++ AdHeroes.py
@@ -1,3 +1,4 @@
+
 #AdHeroes
 from pygame import *
 from math import *
@@ -35,26 +36,59 @@ def menu():
                           
 def select():
     running = True
+    mapPos=0
+    mapList=[]#we need to add map
     #background image
     screen.fill((30,30,24))
     selectbox=[Rect(x*120+170,510,100,100) for x in range(6)]
+    mapRect=Rect(270,210,500,200)
+    rightRect=Rect(870,260,50,100)
+    leftRect=Rect(120,260,50,100)
+    '''using textures.py for showing and changing map
+    '''
     while running:
         for evnt in event.get():          
             if evnt.type == QUIT:
                 running = False
+        mb=mouse.get_pressed()
+        mx,my=mouse.get_pos()
         for i in range(len(selectbox)):
             draw.rect(screen,(150,100,200),selectbox[i])
-        draw.rect(screen,(200,200,120),(270,210,500,200))#showing map
-        draw.rect(screen,(110,200,100),(120,260,50,100))
-        draw.rect(screen,(110,200,100),(870,260,50,100))
+        draw.rect(screen,(200,200,120),mapRect)#showing map
+        draw.rect(screen,(110,200,100),leftRect)
+        draw.rect(screen,(110,200,100),rightRect)
         if key.get_pressed()[27]: running = False
+        if mb[0]==1 and rightRect.collidepoint(mx,my):
+            mapPos=(mapPos+1)%len(mapList)
+        if mb[0]==1 and leftRect.collidepoint(mx,my):
+            mapPos=(mapPos-1)%len(mapList)
+        #resize the map as mapRect size
+        screen.blit(mapList[mapPos],(270,210))
         display.flip()
     return "menu"
 
 def instructions():
     running = True
-    
-    
+    #screen.load() load picture
+    while running:
+        for evnt in event.get():
+            if evnt.type == QUIT:
+                running = False
+        #screen.blit()#blit the instruction picture
+        if key.get_pressed()[27]: running = False
+        display.flip()
+    return "menu"
+
+def credit():
+    running = True
+    while running:
+        for evnt in event.get():
+            if evnt.type == QUIT:
+                running = False
+        #screen.blit()#blit the credit picture
+        if key.get_pressed()[27]: running = False
+        display.flip()
+    return "menu"
 running=True                          
 x,y=0,0        
 page = "menu"
@@ -64,9 +98,7 @@ while page != "exit":
     if page == "game":
         page = simpleGame()    
     if page == "instructions":
-        page = instructions()    
-    if page == "story":
-        page = story()    
+        page = instructions()       
     if page == "credits":
         page = credit()
     if page == "select":
